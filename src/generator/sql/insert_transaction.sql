4dc2af28a4fd21deb9fb8b1e87004bf148246f80
mwewior <mikolaj.wewior@gmail.com>
2026-04-28 02:36:36 +0200

    fix: resolved DB errors, added new service
    Moved old _db_seeder.py_ to seeder/ directory and renamed to main.py - works as a new service that populates the DB at the start of the system; adjusted the compose.yaml; moved SQL queries from python files to seperate .sql files

--- src/generator/sql/insert_transaction.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO transactions (transaction_id, client_id, receiver_id, timestamp_iso, transaction_type, \
-                                              channel, amount, currency, location, ip_address, mac_address, \
-                                              fingerprint, session_id, timestamp_ms, current_state, history, \
-                                              client_type, is_fraud, fraud_reason) \
-                    VALUES %s
